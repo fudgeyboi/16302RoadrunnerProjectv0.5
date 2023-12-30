4a82740a7c70070f036c2dcda237ed865fa253b3
fudgeyboi <gaugevm@gmail.com>
2023-12-29 20:04:14 -0600

    Code update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/AutoCoyoteV3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/AutoCoyoteV3.java
@@ -7,7 +7,6 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
