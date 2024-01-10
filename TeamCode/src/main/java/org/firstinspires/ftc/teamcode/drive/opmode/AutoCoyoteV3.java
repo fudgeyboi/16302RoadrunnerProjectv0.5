c7ebfa97ef79417f91d4ea1a6bac0dbd963f9676
Gauge-Laptop <gaugevm@gmail.com>
2024-01-10 17:47:04 -0600

    Minor update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/AutoCoyoteV3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/AutoCoyoteV3.java
@@ -4,6 +4,8 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.VoltageSensor;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
@@ -18,9 +20,16 @@ public class AutoCoyoteV3 extends TeleOp {
     public void runOpMode() throws InterruptedException {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
         drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        arm = hardwareMap.get(DcMotorEx.class, "arm");
+        launch = hardwareMap.get(Servo.class, "launch");
+        liftkit = hardwareMap.get(DcMotor.class, "beesechurger");
+        slide = hardwareMap.get(DcMotor.class, "slide");
+        llaunch = hardwareMap.get(Servo.class, "llaunch");
+        lift = hardwareMap.get(Servo.class, "lift");
+        ControlHub_VoltageSensor = hardwareMap.get(VoltageSensor.class, "Control Hub");
         telemetry.addLine("Ready");
         telemetry.update();
-
+        drive.setPoseEstimate(new Pose2d(-60,12,Math.toRadians(0)));
         TrajectorySequence trajsq = drive.trajectorySequenceBuilder(new Pose2d(-60,12,Math.toRadians(0)))
                 .lineToSplineHeading(new Pose2d(-36,48,Math.toRadians(-90)))
                 .addDisplacementMarker(() -> {
@@ -28,8 +37,9 @@ public class AutoCoyoteV3 extends TeleOp {
                     arm.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
                     arm.setVelocity(Math.toRadians(720), AngleUnit.RADIANS);
                 })
-                .addTemporalMarker(() -> {
+                .addTemporalMarker(4,() -> {
                     claw.setPosition(0);
+                    eintake.setPosition(1);
                 })
                 .lineToSplineHeading(new Pose2d(-60,48,Math.toRadians(-90)))
                 .lineToSplineHeading(new Pose2d(-60,65,Math.toRadians(-90)))
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/TeleOp.java
@@ -2,13 +2,12 @@ package org.firstinspires.ftc.teamcode.drive.opmode;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
 
-import org.firstinspires.ftc.robotcore.external.JavaUtil;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
@@ -19,12 +18,15 @@ public class TeleOp extends LinearOpMode {
 
     public DcMotorEx arm;
     public Servo claw;
-    private Servo launch;
-    private DcMotor beesechurger;
-    private DcMotor slide;
-    private Servo llaunch;
-    private Servo lift;
-    private VoltageSensor ControlHub_VoltageSensor;
+    public Servo launch;
+    public DcMotor liftkit;
+    public DcMotor slide;
+    public Servo llaunch;
+    public Servo lift;
+    public VoltageSensor ControlHub_VoltageSensor;
+    public Servo eintake;
+    public CRServo intake;
+    private int armExtend;
 
     /**
      * This function is executed when this Op Mode is selected from the Driver Station.
@@ -36,56 +38,51 @@ public class TeleOp extends LinearOpMode {
         drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         arm = hardwareMap.get(DcMotorEx.class, "arm");
         launch = hardwareMap.get(Servo.class, "launch");
-        beesechurger = hardwareMap.get(DcMotor.class, "beesechurger");
+        liftkit = hardwareMap.get(DcMotor.class, "beesechurger");
         slide = hardwareMap.get(DcMotor.class, "slide");
         llaunch = hardwareMap.get(Servo.class, "llaunch");
         lift = hardwareMap.get(Servo.class, "lift");
         ControlHub_VoltageSensor = hardwareMap.get(VoltageSensor.class, "Control Hub");
+        eintake = hardwareMap.get(Servo.class, "eintake");
+        intake = hardwareMap.get(CRServo.class, "intake");
+        armExtend = 0;
 
         // Put initialization blocks here.
         arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         launch.setDirection(Servo.Direction.FORWARD);
-        arm.setDirection(DcMotorEx.Direction.REVERSE);
-        arm.setPositionPIDFCoefficients(2);
+        arm.setVelocityPIDFCoefficients(2,2,3,1);
+        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         waitForStart();
         if (opModeIsActive()) {
             telemetry.setDisplayFormat(Telemetry.DisplayFormat.CLASSIC);
+            eintake.setPosition(0);
             // Put run blocks here.
             while (opModeIsActive()) {
                 // Put loop blocks here.
-                if (gamepad1.right_stick_button && gamepad1.left_stick_button) {
-                    drive.setWeightedDrivePower(
-                            new Pose2d(
-                                    -gamepad1.left_stick_y,
-                                    -gamepad1.left_stick_x,
-                                    -gamepad1.right_stick_x
-                            )
+                drive.setWeightedDrivePower(
+                        new Pose2d(
+                                -gamepad1.left_stick_y,
+                                -gamepad1.right_stick_x,
+                                -gamepad1.left_stick_x
+                        )
                     );
 
-                    drive.update();
-                } else {
-                    drive.setWeightedDrivePower(
-                            new Pose2d(
-                                    -gamepad1.left_stick_y,
-                                    -gamepad1.right_stick_x,
-                                    -gamepad1.left_stick_x
-                            )
-                    );
+                drive.update();
+                liftkit.setPower(-gamepad1.right_stick_y);
+                slide.setPower(gamepad2.right_trigger);
+                arm.setVelocity(120*gamepad2.left_stick_y,AngleUnit.DEGREES);
+                /*armExtend += 50*gamepad2.left_stick_y;
+                if (armExtend > 750) {
+                    armExtend = 750;
+                } else if (armExtend <= 10) {
+                    armExtend = 10;
+                }
+                arm.setTargetPosition(armExtend);
+                arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                arm.setVelocity(Math.toRadians(1080), AngleUnit.RADIANS);*/
 
-                    drive.update();
-                }
-                beesechurger.setPower(gamepad1.right_stick_y);
-                slide.setPower(gamepad2.right_stick_y);
-                if (gamepad2.dpad_up) {
-                    arm.setTargetPosition(800);
-                    arm.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-                    arm.setVelocity(Math.toRadians(720), AngleUnit.RADIANS);
-                } else if (gamepad2.dpad_down) {
-                    arm.setTargetPosition(40);
-                    arm.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-                    arm.setVelocity(Math.toRadians(720), AngleUnit.RADIANS);
-                }
-                if (gamepad1.right_trigger >= 0.9) {
+                if (gamepad2.right_trigger >= 0.9) {
                     launch.setPosition(0);
                 }
                 if (gamepad2.a) {
@@ -100,9 +97,10 @@ public class TeleOp extends LinearOpMode {
                 if (gamepad1.b) {
                     lift.setPosition(0);
                 }
-                telemetry.addData("Arm Position (TICKS", arm.getCurrentPosition());
+                telemetry.addData("Arm Position (TICKS)", arm.getCurrentPosition());
                 telemetry.addData("Arm Current Draw", arm.getCurrent(CurrentUnit.AMPS));
                 telemetry.addData("Voltage", ControlHub_VoltageSensor.getVoltage());
+                telemetry.addData("ArmVar", armExtend);
                 if (gamepad1.back || gamepad2.back) {
                     requestOpModeStop();
                 }
